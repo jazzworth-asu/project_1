57fe9fc37642c76597b8eeeeafcd1dd74a4afe70
jazzworth-asu <jdashwo2@asu.edu>
2021-05-24 15:58:02 -0600

    implemented drag and drop in "current work" tab

--- script.js
+++ script.js
@@ -65,7 +65,28 @@ $(document).ready(function () {
         let state = ($(this).val());
     });
 
+  
+
+
 });
+
+  /*drag and drop*/
+  function allowDrop(ev)
+  {
+  ev.preventDefault();
+  }
+
+  function drag(ev)
+  {
+  ev.dataTransfer.setData("text",ev.target.id);
+  }
+
+  function drop(ev)
+  {
+  ev.preventDefault();
+  var data=ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+  }
 
 function getLocation() {
 
